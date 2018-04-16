e8152a2587d1b8145667fba8ca9fb9e073a3af8a
DEV3L <jus.beall@gmail.com>
2018-04-16 10:06:24 -0400

    Add inline CSS styling
    End 'How to Create Custom React Components'
    https://www.youtube.com/watch?v=bqckLTOwdwg

--- src/components/ChordEditor.js
+++ src/components/ChordEditor.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
+import ChordSheetJS from 'chordsheetjs';
 
 class ChordEditor extends Component {
     constructor(props) {
         super(props);
-        this.state =  { year: new Date().getFullYear() };
+        this.handleChange = this.handleChange.bind(this);
+        this.state = { value: 'Type some lyrics here'}
+    }
+
+    handleChange(e) {
+        this.setState( { value: e.target.value});
+    }
+
+    getChordMarkup() {
+        let formatter = new ChordSheetJS.HtmlDivFormatter(),
+            parser = new ChordSheetJS.ChordProParser(),
+            song = parser.parse(this.state.value);
+
+        return {__html: formatter.format(song) };
     }
 
     render() {
         return (
-            <h3>Editor</h3>
+            <div className="chord-editor">
+                <div className="panel">
+                    <h3>Input</h3>
+                    <textarea
+                        style={{width: "100%", height: "100%"}}
+                        onChange={this.handleChange}
+                        defaultValue={this.state.value} />
+                </div>
+                <div className="panel">
+                    <h3>Output</h3>
+                    <div
+                        style={{width: "100%", height: "100%", fontFamily: "monospace"}}
+                        className="chord-output"
+                        dangerouslySetInnerHTML={this.getChordMarkup()} />
+                </div>
+            </div>
         );
     }
 }
