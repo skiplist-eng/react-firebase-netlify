3043ad94709a825034049cb80461fa2e54fe41c9
DEV3L <jus.beall@gmail.com>
2018-04-20 19:36:57 -0400

    Add breadcrumbs to songs

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,14 +8,27 @@ class Header extends Component {
             <nav className="pt-navbar">
                 <div className="pt-navbar-group pt-align-left">
                     <div className="pt-navbar-heading">Skiplist Starter Kit</div>
-                    <input className="pt-input" placeholder="Search songs..." type="text"/>
+                    {this.props.authenticated
+                        ? <input className="pt-input" placeholder="Search songs..." type="text"/>
+                        : null}
                 </div>
-                <div className="pt-navbar-group pt-align-right">
-                    <Link className="pt-button pt-minimal pt-icon-music" to="/songs">Songs</Link>
-                    <span className="pt-navbar-divider"/>
-                    <button className="pt-button pt-minimal pt-icon-user"/>
-                    <button className="pt-button pt-minimal pt-icon-cog"/>
-                </div>
+                {this.props.authenticated
+                    ? (
+                        <div className="pt-navbar-group pt-align-right">
+                            <Link className="pt-button pt-minimal pt-icon-music" to="/songs">Songs</Link>
+                            <span className="pt-navbar-divider"/>
+                            <button className="pt-button pt-minimal pt-icon-user"/>
+                            <button className="pt-button pt-minimal pt-icon-cog"/>
+                        </div>
+                    )
+                    : (
+                        <div className="pt-navbar-group pt-align-right">
+                            <Link className="pt-button pt-intent-primary" to="/login">
+                                Register/Log In
+                            </Link>
+                        </div>
+                    )
+                }
             </nav>
         );
     }
